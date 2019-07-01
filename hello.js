aa871c798ecc7a5f6cad2ccdf80a8e710337d78b
tianmeng <1107210976@qq.com>
2019-07-01 17:35:14 +0800

    提交

---
+++ hello.js
@@ -0,0 +1,8 @@
+function sayHello(person) {
+    return 'Hello, ' + person;
+}
+var user = "Json";
+var tom = {
+    name: 'Tom',
+    age: 25
+};
---
+++ hello.ts
@@ -0,0 +1,14 @@
+function sayHello(person: string) {
+    return 'Hello, ' + person;
+}
+let user = "Json";
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+let tom: Person = {
+    name: 'Tom',
+    age: 25
+};
